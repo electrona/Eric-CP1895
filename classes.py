3b298760af837310a188d8182753c26b6caa21ff
Eric Thomas <ericthomas@tuta.io>
2022-01-26 14:48:30 -0330

    classes fixes

--- classes.py
+++ classes.py
@@ -7,17 +7,19 @@ class Lightbulb:
         self.is_on = is_on
 
     def turn_on(self):
+        self.is_on = True
         print("Light is on!")
 
     def turn_off(self):
+        self.is_on = False
         print("Light is off")
 
     def to_string(self):
         print(f"Wattage: {self.wattage}")
         print(f"Brightness: {self.brightness}")
-        print(f"Is LED: {self.wattage}")
-        print(f"Brand: {self.wattage}")
-        print(f"Is on: {self.wattage}")
+        print(f"Is LED: {self.is_led}")
+        print(f"Brand: {self.brand}")
+        print(f"Is on: {self.is_on}")
 
     def set_brightness(self, brightness):
         self.brightness = brightness
@@ -25,8 +27,8 @@ class Lightbulb:
 
 
 def main():
-    pass
-
+    lightbulb = Lightbulb(100, 100, True, "Philips")
+    lightbulb.to_string()
 
 if __name__ == "__main__":
     main()
